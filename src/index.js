58ac0f6eb5039a3345d17931e25991e3ef787456
Julian Barker <julian.c.barker@gmail.com>
2023-01-11 22:07:21 -0800

    feat: add auth and RBAC

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { MantineProvider } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SettingsProvider from './Context/Settings';
+import AuthProvider from './Context/Auth';
 
 import App from './App';
 
@@ -9,9 +10,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <SettingsProvider>
-        <App />
-      </SettingsProvider>
+      <AuthProvider>
+        <SettingsProvider>
+          <App />
+        </SettingsProvider>
+      </AuthProvider>
     </MantineProvider>
   </React.StrictMode>
 );
